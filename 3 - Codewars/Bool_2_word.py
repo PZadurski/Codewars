34b3a848fd3d28b73c82494b39f5af1e790e6411
Patryk Zadurski <patryk.zadurski@gmail.com>
2024-06-11 10:49:41 +0200

    Bool to txt

---
+++ 3 - Codewars/Bool_2_word.py
@@ -0,0 +1,10 @@
+def bool_to_word(boolean):
+    if boolean == True:
+        return('Yes')
+    if boolean == False:
+        return('No')
+
+#def bool_to_word(bool):
+#return "Yes" if bool else "No"
+
+bool_to_word(True)
